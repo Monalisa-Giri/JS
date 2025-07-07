ed2ea85eb1f03b21efabb1c64fdd42ee6fc86da9
Monalisa-Giri <girimonalisa57@gmail.com>
2025-07-07 13:27:18 +0000

    Data Types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"user strict";  //treat all JS code as newer version
+
+// alert(3 + 3)  //weareusing nodejs,not browser
+
+console.log(3 + 3) //code readability should be high
+
+console.log("Water")
+
+let name = "Mona"
+let age = 22
+let isLoggedIn = false
+
+// number=> 2 to power 53
+// bigint
+// String=> ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+//object
+
+console.log(typeof age);
+console.log(typeof "hitesh");
+
+console.log(typeof undefined); // undefined
+console.log(typeof null);// object
